1008e676f622507da1a15c4f9eb7b07161845cfc
arielglvz <arielglvz@gmail.com>
2025-01-09 21:37:16 +0800

    preparing for deployment

--- server.js
+++ server.js
@@ -9,8 +9,17 @@ app.use(express.json());
 
 app.use("/api/portfolio", portfolioRoute);
 
-// const port = process.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
+// const port = 5000;
+const path = require("path");
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));;
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
